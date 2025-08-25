8e770f52975902476b8a39a010db90200d916b83
Jeongwoo Kim <corc411@gmail.com>
2025-08-25 18:56:33 +0900

    feat: add CustomButton component and integrate it into HomeScreen
    - Created `CustomButton` component with customizable label, size, and variant props.
    - Added `CustomButton` to the HomeScreen for improved UI interaction.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,12 @@
 import { SafeAreaView, Text } from 'react-native';
 
+import { CustomButton } from '@/components/CustomButton';
+
 export default function HomeScreen() {
   return (
     <SafeAreaView>
       <Text>홈스크린</Text>
+      <CustomButton label="버튼" onPress={() => {}} />
     </SafeAreaView>
   );
 }
---
+++ components/CustomButton.tsx
@@ -0,0 +1,50 @@
+import { Pressable, PressableProps, StyleSheet, Text } from 'react-native';
+
+import { colors } from '@/constants';
+
+interface CustomButtonProps extends PressableProps {
+  label: string;
+  size?: 'medium' | 'large';
+  variant?: 'filled';
+}
+
+export function CustomButton({
+  label,
+  size = 'large',
+  variant = 'filled',
+  ...props
+}: CustomButtonProps) {
+  return (
+    <Pressable
+      style={({ pressed }) => [
+        styles.containter,
+        styles[size],
+        styles[variant],
+        pressed && styles.pressed,
+      ]}
+      {...props}
+    >
+      <Text style={styles[variant]}>{label}</Text>
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  containter: {
+    borderRadius: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  large: {
+    width: '100%',
+    height: 44,
+  },
+  medium: {},
+  filled: {
+    backgroundColor: colors.ORANGE_600,
+    fontSize: 14,
+    fontWeight: 'bold',
+    color: colors.WHITE,
+  },
+  pressed: { opacity: 0.8 },
+});
