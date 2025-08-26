b87d3b599afb46636a102f91f96f433a4ae38d18
Jeongwoo Kim <corc411@gmail.com>
2025-08-26 19:21:19 +0900

    feat: add FeedList component and update HomeScreen to display feed list
    - Created `FeedList` component to render a list of feed items using dummy data.
    - Updated HomeScreen to use `FeedList` instead of a single `FeedItem`.
    - Applied container styling and background color.

---
+++ components/FeedList.tsx
@@ -0,0 +1,65 @@
+import { FlatList, StyleSheet } from 'react-native';
+
+import { colors } from '@/constants';
+
+import { FeedItem } from './FeedItem';
+
+const dummyData = [
+  {
+    id: 1,
+    userId: 1,
+    title: '더미 제목입니다.',
+    description:
+      '더미 내용입니다. 더미 내용입니다. 더미 내용입니다. 더미 내용입니다. 더미 내용입니다. 더미 내용입니다. 더미 내용입니다.  더미 내용입니다.더미 내용입니다.  더미 내용입니다. 더미 내용입니다.',
+    createdAt: '',
+    author: {
+      id: 1,
+      nickname: '닉네임',
+      imageUri: '',
+    },
+    imageUris: [],
+    likes: [],
+    hasVote: false,
+    voteCount: 1,
+    commentCount: 1,
+    viewCount: 1,
+  },
+  {
+    id: 2,
+    userId: 1,
+    title: '더미 제목입니다.',
+    description:
+      '더미 내용입니다. 더미 내용입니다. 더미 내용입니다. 더미 내용입니다. 더미 내용입니다. 더미 내용입니다. 더미 내용입니다.  더미 내용입니다.더미 내용입니다.  더미 내용입니다. 더미 내용입니다.',
+    createdAt: '',
+    author: {
+      id: 1,
+      nickname: '닉네임',
+      imageUri: '',
+    },
+    imageUris: [],
+    likes: [],
+    hasVote: false,
+    voteCount: 1,
+    commentCount: 1,
+    viewCount: 1,
+  },
+];
+
+export function FeedList() {
+  return (
+    <FlatList
+      data={dummyData}
+      renderItem={({ item }) => <FeedItem post={item} />}
+      keyExtractor={(item) => String(item.id)}
+      contentContainerStyle={styles.contentContainer}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  contentContainer: {
+    paddingVertical: 12,
+    backgroundColor: colors.GRAY_200,
+    gap: 12,
+  },
+});
