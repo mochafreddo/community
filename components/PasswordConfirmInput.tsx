1d5cbba5c437c0e73d3d47b4a9619b4b2c00e3f6
Jeongwoo Kim <corc411@gmail.com>
2025-08-28 23:34:24 +0900

    feat: add validation rules and error messages to email, password, and password confirmation inputs
    - Added validation for email format and required field in EmailInput
    - Added minimum length validation and error display for PasswordInput
    - Added password match validation and error display for PasswordConfirmInput

--- components/PasswordConfirmInput.tsx
+++ components/PasswordConfirmInput.tsx
@@ -1,20 +1,30 @@
-import { Controller, useFormContext } from 'react-hook-form';
+import { Controller, useFormContext, useWatch } from 'react-hook-form';
 
 import { InputField } from './InputField';
 
 export const PasswordConfirmInput = () => {
   const { control } = useFormContext();
+  const { password } = useWatch({ control, name: 'password' });
 
   return (
     <Controller
       name="passwordConfirm"
       control={control}
-      render={({ field: { onChange, value } }) => (
+      rules={{
+        validate: (data: string) => {
+          if (data !== password) {
+            return '비밀번호가 일치하지 않습니다.';
+          }
+        },
+      }}
+      render={({ field: { onChange, value }, fieldState: { error } }) => (
         <InputField
           label="비밀번호 확인"
           placeholder="비밀번호 확인을 입력해 주세요."
+          secureTextEntry
           value={value}
           onChangeText={onChange}
+          error={error?.message}
         />
       )}
     />
